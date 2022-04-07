841a06b937795586c986e95962f1366fd9b61327
badger <badger@badger.local>
2022-04-07 21:44:42 +0300

    Implementing JWT on the front end. Adding JWTs to sign-up page

--- front-end/src/pages/SignUpPage.js
+++ front-end/src/pages/SignUpPage.js
@@ -1,7 +1,11 @@
 import {useState} from "react";
 import {useHistory} from "react-router-dom";
+import axios from "axios";
+
+import {useToken} from "../auth/useToken";
 
 export const SignUpPage = () => {
+	const [token, setToken] = useToken();
 	const [errorMessage, setErrorMessage] = useState('');
 	const [emailValue, setEmailValue] = useState('');
 	const [passwordValue, setPasswordValue] = useState('')
@@ -10,7 +14,13 @@ export const SignUpPage = () => {
 	const history = useHistory();
 
 	const onSignUpClicked = async () => {
-		alert('Sign up is not implemented yet')
+		const response = await axios.post('/api/signup', {
+			emai: emailValue,
+			password: passwordValue,
+		});
+		const {token} = response.data;
+		setToken(token);
+		history.push('/')
 	}
 	return (
 		<div className="content-container">
