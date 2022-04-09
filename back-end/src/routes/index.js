389b77601765300414c641f2418360dba07b24b6
badger <badger@badger.local>
2022-04-09 18:07:23 +0300

    Adding a verify email route

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,10 +2,12 @@ import { loginRoute } from './LoginRoute';
 import { signUpRoute } from './SignUpRoute';
 import { testRoute } from './testRoute';
 import {updateUserInfoRoute} from "./updateUserInforRoute";
+import {verifyEmailRoute} from "./verifyEmail";
 
 export const routes = [
     testRoute,
     signUpRoute,
     loginRoute,
     updateUserInfoRoute,
+    verifyEmailRoute,
 ];
